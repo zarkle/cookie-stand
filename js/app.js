506b4c55a20d070eb04eeaed7b6704f4214db3eb
Beverly Pham <beverly.pham@gmail.com>
2018-01-16 14:10:28 -0800

    table render (first column) added

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 // var allStores = [];
-// var storeTable = document.getElementById('table');
+var salesTable = document.getElementById('sales-table');
 
-function Store(minHourlyCust, maxHourlyCust, avgCookiesPerCust) {
+function Store(location, minHourlyCust, maxHourlyCust, avgCookiesPerCust) {
+  this.location = location;
   this.minHourlyCust = minHourlyCust;
   this.maxHourlyCust = maxHourlyCust;
   this.avgCookiesPerCust = avgCookiesPerCust;
@@ -29,23 +30,18 @@ Store.prototype.salesPerHour = function() {
 };
 
 Store.prototype.render = function() {
-  this.salesPerHour();
-  var ulEl = document.getElementById('first-and-pike');
-  for (var j = 0; j < hours.length; j++) {
-    var liEl = document.createElement('li');
-    liEl.textContent = hours[j] + ': ' + this.hourlySales[j] + ' cookies';
-    ulEl.appendChild(liEl);
-  }
-  liEl = document.createElement('li');
-  liEl.textContent = 'Total: ' + this.totalCookies + ' cookies';
-  ulEl.appendChild(liEl);
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.location;
+  trEl.appendChild(tdEl);
+  salesTable.appendChild(trEl);
 };
 
-var firstAndPikeStore = new Store(23, 65, 6.3);
-var seatacAirportStore = new Store(3, 24, 1.2);
-var seattleCenterStore = new Store (11, 38, 3.7);
-var capitolHillStore = new Store(20, 38, 2.3);
-var alkiStore = new Store(2, 16, 4.6);
+var firstAndPikeStore = new Store('1st and Pike', 23, 65, 6.3);
+var seatacAirportStore = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenterStore = new Store ('Seattle Center', 11, 38, 3.7);
+var capitolHillStore = new Store('Capitol Hill', 20, 38, 2.3);
+var alkiStore = new Store('Alki', 2, 16, 4.6);
 
 firstAndPikeStore.render();
 seatacAirportStore.render();
