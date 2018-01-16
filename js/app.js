b29678c9ae3d5e6c5b0d7d3805ac60df0e48079e
Beverly Pham <beverly.pham@gmail.com>
2018-01-16 14:22:02 -0800

    table header row added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 // var allStores = [];
 var salesTable = document.getElementById('sales-table');
 
@@ -37,12 +37,26 @@ Store.prototype.render = function() {
   salesTable.appendChild(trEl);
 };
 
+function makeHeaderRow() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = '';
+  trEl.appendChild(thEl);
+  for (var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  salesTable.appendChild(trEl);
+}
+
 var firstAndPikeStore = new Store('1st and Pike', 23, 65, 6.3);
 var seatacAirportStore = new Store('SeaTac Airport', 3, 24, 1.2);
 var seattleCenterStore = new Store ('Seattle Center', 11, 38, 3.7);
 var capitolHillStore = new Store('Capitol Hill', 20, 38, 2.3);
 var alkiStore = new Store('Alki', 2, 16, 4.6);
 
+makeHeaderRow();
 firstAndPikeStore.render();
 seatacAirportStore.render();
 seattleCenterStore.render();
